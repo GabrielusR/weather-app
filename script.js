658b75911e691c84f2569f6433f07d3b55f2e4de
GabrielR <gabrielramos1510@gmail.com>
2016-05-24 19:00:46 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,78 @@
+//script.js
+var APPID = "4ffc70eb22e7108feb842ad56cb45d4c";
+var temp;
+var loc;
+var icon;
+var humidity;
+var wind;
+var direction;
+
+function updateByCity(city) {
+    var url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&lang=pt" + "&APPID=" + APPID;
+    sendRequest(url);
+}
+
+function sendRequest(url) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+      if(xmlhttp.readyState === 4 && xmlhttp.status === 200)   {
+            var data = JSON.parse(xmlhttp.responseText);
+            var weather = {};
+            weather.icon = data.weather[0].id;
+            weather.humidity = data.main.humidity;
+            weather.wind = ms2kmh(data.wind.speed);
+            weather.direction = deg2dir(data.wind.deg);
+            weather.loc = data.name;
+            weather.temp = k2c(data.main.temp);
+          
+            update(weather);
+      }
+    };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
+
+function ms2kmh (msspeed) {
+    return Math.round(msspeed*3.6);
+}
+
+function deg2dir(deg) {
+    var range = 360/16;
+    var low = 360 - range/2;
+    var high = (low + range) % 360;
+    var angles = ["N", "NNE", "LNE", "L", "LSE", "SE", "SSE", "S", "SSO", "SO", "OSO", "ONO", "NNO"];
+    for (i in angles) {
+        if(deg >= low && deg < high) {
+            return angles[i];
+        }
+        low = (low + range) % 360;
+        high = (high + range) % 360;
+    }
+    
+    return "N";
+}
+
+function k2c(ktemp) {
+    return Math.round(ktemp - 273.15);
+}
+
+function update(weather) {
+    wind.innerHTML = weather.wind;
+    direction.innerHTML = weather.direction;
+    humidity.innerHTML = weather.humidity;
+    loc.innerHTML = weather.loc;
+    temp.innerHTML = weather.temp;
+    icon.src = "imgs/codes/" + weather.icon + ".png";
+}
+
+window.onload = function() {
+    temp = document.getElementById("temperature");
+    loc = document.getElementById("location");
+    icon = document.getElementById("icon");
+    humidity = document.getElementById("humidity");
+    wind = document.getElementById("wind");
+    direction = document.getElementById("direction");
+
+    
+    updateByCity("Rio de Janeiro");
+}
